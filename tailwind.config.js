f4019a0eb6e807211e5ac3881c202119444515f0
Lokesh Kumar <lokeshkumar.080806@gmail.com>
2024-09-03 10:30:22 +0530

    adding neumorphism to the buttons in gallery component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        "light-theme-gray": "#bfbfbf",
+        "light-button-gray": "#bfbfbf",
+        "light-font": "#474747"
+      },
+      borderRadius: {
+        card: "10px"
+      },
+      boxShadow: {
+        "light-card":
+          "12px 12px 16px 0 rgba(0, 0, 0, 0.3), -8px -8px 12px 0 rgba(0, 0, 0, .25)",
+        "light-button":
+          "6px 6px 8px 0 rgba(0, 0, 0, 0.3), -4px -4px 6px 0 rgba(0, 0, 0, .25)"
+      },
+      height: {
+        card: "300px",
+        button: "40px"
+      },
+      width: {
+        card: "250px",
+        button: "100px"
+      }
+    }
+    },
   plugins: [],
 }
 
